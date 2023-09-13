9786cf9c222c031700ace4d54f2638dd8621af78
Jatin Sharma <mca22.jatin.sharma@sunstone.edu.in>
2023-09-13 23:47:40 +0530

    Done with Passcode Design

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const menu_parent = document.querySelector(".hidden__menu");
 const crossBtn = document.querySelector(".cross");
 const menuColumns = document.querySelectorAll(".column");
 const allImages = document.querySelectorAll("img");
+const passcodeBtn = document.querySelector(".passcode");
+const allColumns = document.querySelectorAll(".columnTitle");
 
 text_parent.addEventListener("click", () => {
   text_parent.style.display = "none";
@@ -37,5 +39,40 @@ allImages.forEach((image) => {
     image.style.cursor = "pointer";
 
     image.style.filter = "none";
+  });
+});
+
+function optionsDisable() {
+  document.querySelectorAll(".rows").forEach((ele) => {
+    ele.style.display = "none";
+  });
+}
+const mediaWork = window.matchMedia("(max-width:800px)");
+console.log(mediaWork);
+setTimeout(() => {
+  allColumns.forEach((headings, i) => {
+    const target = headings.closest(".column").querySelector(".rows");
+    console.log(target);
+    if (mediaWork.matches) {
+      headings.addEventListener("click", () => {
+        optionsDisable();
+
+        target.style.display = "flex";
+      });
+    }
+  });
+}, 1000);
+
+passcodeBtn.addEventListener("click", () => {
+  menuColumns.forEach((ele) => {
+    ele.style.display = "none";
+  });
+  document.querySelector(".passcodeForm").style.display = "flex";
+});
+
+document.querySelector("#exitPasscode").addEventListener("click", () => {
+  document.querySelector(".passcodeForm").style.display = "none";
+  menuColumns.forEach((ele) => {
+    ele.style.display = "flex";
   });
 });
